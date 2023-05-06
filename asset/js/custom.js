6f5d03ec00b1ff5bbe83f7d86ed27eba3e413b1a
mahdisanaeifar <mahdisanaeifar@gmail.com>
2023-05-06 22:56:48 +0330

    events-1

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,11 +1,18 @@
 var myApplication = new Vue({
     el: '#app',
     data: {
-        title: 'Hello Vue.js'
+        title: 'Hello Vue.js',
+        link: 'http://toplearn.com'
     },
     methods: {
         changeTitle: function (event) {
             this.title = event.target.value;
+        },
+        sayHello: function () {
+            return 'Hello Toplearn';
+        },
+        sayBye: function () {
+            return this.title;
         }
     }
 });

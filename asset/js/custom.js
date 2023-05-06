e2ba6358544497f73c91de413749a5fc600d92c5
mahdisanaeifar <mahdisanaeifar@gmail.com>
2023-05-06 22:59:16 +0330

    events-2

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,18 +1,40 @@
-var myApplication = new Vue({
+
+// Events 1
+// var myApplication = new Vue({
+//     el: '#app',
+//     data: {
+//         title: 'Hello Vue.js',
+//         link: 'http://toplearn.com'
+//     },
+//     methods: {
+//         changeTitle: function (event) {
+//             this.title = event.target.value;
+//         },
+//         sayHello: function () {
+//             return 'Hello Toplearn';
+//         },
+//         sayBye: function () {
+//             return this.title;
+//         }
+//     }
+// });
+
+// Events 2
+new Vue({
     el: '#app',
     data: {
-        title: 'Hello Vue.js',
-        link: 'http://toplearn.com'
+        counter: 5,
+        x: 0,
+        y: 0
     },
     methods: {
-        changeTitle: function (event) {
-            this.title = event.target.value;
+        Increase: function (step, obj) {
+            debugger;
+            this.counter += step;
         },
-        sayHello: function () {
-            return 'Hello Toplearn';
-        },
-        sayBye: function () {
-            return this.title;
+        GetCursor: function (event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
         }
-    }
+    },
 });

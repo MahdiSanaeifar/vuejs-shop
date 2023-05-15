291f4624af5aceb3d8421120403f329cafeb4edf
mahdisanaeifar <mahdisanaeifar@gmail.com>
2023-05-15 21:37:12 +0330

    working with Directive and modifires

--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,12 @@
 <template>
   <div class="container">
-    <h1 id="header">Form</h1>
-    <hr>
     <div class="row">
-      <div class="col-md-6">
-        <div class="panel panel-primary">
-          <div class="panel-heading">Form Inputs :</div>
-          <div class="panel-body">
-            <div class="form-group">
-              <label for="email">Email</label>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                class="form-control"
-                placeholder="enter your email"
-                v-model.lazy="user.email"
-              >
-            </div>
-            <div class="form-group">
-              <label for="age">Age</label>
-              <input
-                type="number"
-                name="age"
-                id="age"
-                class="form-control"
-                placeholder="enter your age"
-                v-model="user.age"
-              >
-            </div>
-            <div class="form-group">
-              <label>Password</label>
-              <input
-                type="password"
-                name="password"
-                class="form-control"
-                placeholder="enter your password"
-                v-model="user.password"
-              >
-            </div>
-            <hr>
-            <div class="form-group">
-              <label>Message</label>
-              <textarea class="form-control" v-model="message"></textarea>
-            </div>
-            <hr>
-            <div class="col-md-4">
-              <div class="form-group">
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox" value="ReciveMail" v-model="recieveTypes"> Recive Mail
-                  </label>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="form-group">
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox" value="ReciveMessages" v-model="recieveTypes"> Recive Messages
-                  </label>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="form-group">
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox" value="ReciveCalls" v-model="recieveTypes"> Recive Calls
-                  </label>
-                </div>
-              </div>
-            </div>
-            <div class="clearfix"></div>
-            <hr>
-            <div class="col-md-4">
-              <div class="form-group">
-                <div class="radio">
-                  <label>
-                    <input type="radio" value="male" v-model="gender"> Male
-                  </label>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="form-group">
-                <div class="radio">
-                  <label>
-                    <input type="radio" value="female" id="ReciveMessages" v-model="gender"> Female
-                  </label>
-                </div>
-              </div>
-            </div>
-            <div class="clearfix"></div>
-            <hr>
-            <div class="form-group">
-              <select id="selectOptions" class="form-control" v-model="selectedPriority">
-                <option v-for="item in priorities" :key="item">{{ item }}</option>
-              </select>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6">
-        <div class="panel panel-primary">
-          <div class="panel-heading">Input Results :</div>
-          <div class="panel-body">
-            <div>
-              <span>Email :</span>
-              <strong>{{ user.email }}</strong>
-            </div>
-
-            <div>
-              <span>Age :</span>
-              <strong>{{ user.age }}</strong>
-            </div>
-
-            <div>
-              <span>Password :</span>
-              <strong>{{ user.password }}</strong>
-            </div>
-
-            <div>
-              <span>TextArea Message :</span>
-              <strong style="white-space:pre">{{ message }}</strong>
-            </div>
-            <hr>
-            <ul>
-              <li v-for="item in recieveTypes" :key="item">{{ item }}</li>
-            </ul>
-            <hr>
-            <div>
-              <span>User Gender :</span>
-              <strong>{{ gender }}</strong>
-            </div>
-            <div>
-              <span>User Priority :</span>
-              <strong>{{ selectedPriority }}</strong>
-            </div>
-          </div>
-        </div>
+      <div class="col-md-12">
+        <h1>Create Custom Directives</h1>
+        <hr>
+        <p v-custom:textcolor.delayed="'yellow'">this paragraph has custom directive</p>
+        <hr>
+        <p v-test="{color:'blue',padding:'30px',fontSize:'2em'}">this paragraph has local directive</p>
       </div>
     </div>
   </div>
@@ -148,34 +14,22 @@
 
 <script>
 export default {
-  data() {
-    return {
-      user: {
-        email: "",
-        age: 22,
-        password: ""
-      },
-      message: "",
-      recieveTypes: [],
-      gender: "male",
-      priorities: ["high", "medium", "low"],
-      selectedPriority: "low"
-    };
+  data() {},
+  directives: {
+    test: {
+      bind(el, binding, vnode) {
+        el.style.padding = binding.value.padding;
+        el.style.color = binding.value.color;
+        el.style.fontSize = binding.value.fontSize;
+        el.style.backgroundColor = "green";
+      }
+    }
   }
 };
 </script>
 
 <style>
-#header {
+h1 {
   text-align: center;
-}
-
-span {
-  color: red;
-  font-size: 1.5em;
-}
-
-textarea {
-  resize: vertical;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+
+Vue.directive('custom', {
+  bind(el, binding, vnode) {
+    var delay = 0;
+
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+
+    setInterval(() => {
+      if (binding.arg === 'textcolor') {
+        el.style.color = binding.value;
+      }
+    }, delay);
+
+    el.style.padding = '20px';
+    el.style.fontSize = '1.5em';
+    el.style.backgroundColor = 'gray';
+  }
+});
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
+
+// Vue.directive('custom', {
+//   bind(el, binding, vnode) {},
+//   inserted(el, binding, vnode) {},
+//   updated(el, binding, vnode, oldVnode) {},
+//   componentUpdated(el, binding, vnode, oldVnode) {},
+//   unbind(el, binding, vnode) {}
+// });
